7cbf431f24cf728398f440d2091796ad0fdf3f31
I-Chiao Hsu <ichiao.hsu@gmail.com>
2019-11-15 18:44:56 +0800

    refactor(readr): rewrite kubernetes logic
    - deprecate osCmd
    - use built-in setTimeout waiting for rollout
    - patch deployment replicas in uploading dist instead of create/delete
    deployment

--- scripts/deploy_readr.js
+++ scripts/deploy_readr.js
@@ -5,273 +5,100 @@ const GCS_BUCKET_PATH = process.env.GCS_BUCKET_PATH
 
 const {initBucket, uploadFileToBucket, readdirAsync, makeFilePublic, } = require('./gcs.js');
 
-const {client, osCmd, } = require('./k8s.js');
+const {client, getDeployVersion, uploadDist, patchDeployment} = require('./k8s.js');
 
 module.exports = function(robot){
   
   robot.respond(/who are you/i, msg => {
-    msg.send("I am Tom")
+    msg.send("I am Tor")
   })
 
-  robot.respond(/version\s+tt\s+(readr-site-mobile|readr-site|news-projects|readr-restful)/i, async msg => {
+  robot.respond(/version\s+dev\s+(readr-site-mobile|readr-site|news-projects|readr-restful)/i, async msg => {
+    let deployName = msg.match[1];
+    
     try {
-      let deployName = msg.match[1];
-      const deploy = await client.apis.apps.v1.namespaces('default').deployments(deployName).get();
-      
-      if (deploy.statusCode === 200) {
-        let containers = deploy.body.spec.template.spec.containers;
-        for (let i=0; i < containers.length ; i++) {
-          if (containers[i].name === deployName) {
-            var version = containers[i].image.slice(containers[i].image.indexOf(":")+1);
-            break;
-          } 
-        }
-        msg.send(`${deployName} is using ${version}`)
-      } else {
-        console.log(deploy.statusCode)
-      }
+      let version = await getDeployVersion("default", deployName)
+      msg.send(`${deployName} is using ${version}`)
     } catch (err) {
-        msg.send(err)
+      msg.send(err)
     }
   });
 
-  robot.respond(/deploy\s+rr\s+(readr-site-mobile|readr-site|news-projects|readr-restful)\s+(.+)/i, async msg => {
-    
+  robot.respond(/deploy\s+dev\s+(readr-site-mobile|readr-site|news-projects-canary|news-projects|readr-restful)\s+(.+)/i, async msg => {
     const deployName = msg.match[1];
-    const version = "gcr.io/mirrormedia-1470651750304/" + deployName + ":" + msg.match[2];
-    const canaryName = deployName + "-canary"
+    const fullImage = "gcr.io/mirrormedia-1470651750304/" + deployName + ":" + msg.match[2];
+    const canaryName = `${deployName}-canary`
+    
+    // Find distribution folder
+    let distribution
 
     switch (deployName) {
       case "readr-site-mobile":
-        var distribution = "distribution"
+        distribution = "distribution"
         break;
       case "readr-site":
-        var distribution = "distribution"
+        distribution = "distribution"
         break;
       case "news-projects":
-        var distribution = "dist"
+        distribution = "dist"
         break;
     }
-
+    // Upload dist if it's not readr-restful
     if (deployName !== "readr-restful"){
-      let canaryTemplate = {
-        apiVersion: "apps/v1",
-        kind: "Deployment",
-        metadata: {
-          name: canaryName,
-          namespace: "dist",
-          labels: {
-            app: canaryName
-          }
-        },
-        spec: {
-          replicas: 1,
-          selector: {
-              matchLabels: {
-              app: canaryName
+      msg.send("launching dist uploading.")
+      try {
+        await uploadDist("dist", canaryName, fullImage, distribution)
+        msg.send("dist uploaded.")
+      }catch (err) {
+        msg.send(err)
+      }
+    }
+  
+    try {
+      await patchDeployment("default", deployName, {
+        body: {
+          spec: {
+            template: {
+              spec: {
+                containers: [{
+                  name: deployName,
+                  image: fullImage,
+                }]
               }
-          },
-          template: {
-            metadata:{
-              labels: {
-                app: canaryName
-              }
-            },
-            spec: {
-              containers: [
-                {
-                  image: version,
-                  name: canaryName
-                }
-              ]
             }
           }
         }
-      }
-      console.log(canaryTemplate)
-  
-      try {
-        const create = await client.apis.apps.v1.namespaces('dist').deployments.post({ body: canaryTemplate })
-      } catch (err) {
-        msg.send(err)
-      }
-    
-      // Watch the creation until it's done
-      osCmd('kubectl', ['rollout', 'status', `deployment/${canaryName}`, '-w', '-n', 'dist'])
-    
-      try {
-        const pod = await client.api.v1.namespaces('dist').pods.get({ qs: {labelSelector: `app=${canaryName}` }})
-        var podName = pod.body.items[0].metadata.name
-        console.log(`pod is named: ${podName}`)
-        // Copy dist file out
-        var distFolder = `/tmp/${podName}/${distribution}`
-        osCmd('kubectl', ['cp', `${podName}:/usr/src/${distribution}`, distFolder, '-n', 'dist'])
-      } catch (err) {
-        msg.send(err)
-      }
-    
-      const bucket = initBucket(GCS_FILE_BUCKET)
-      const destination = GCS_BUCKET_PATH
-      try {
-        files = await readdirAsync(distFolder)
-      } catch (err) {
-        msg.send(err)
-      }
-  
-      let result = await Promise.all(files.map( filename => {
-        return uploadFileToBucket(bucket, `${distFolder}/${filename}`, {
-          destination: `${destination}/${filename}`,
-        }).then((bucketFile) => {
-          console.log(`file ${filename} upload to gcs successfully.`)
-          makeFilePublic(bucketFile)
-        })
-      }))
-      .then(() => {
-        // Remove temporary folder
-        osCmd('rm', ['-rf', `/tmp/${podName}`])
-      })
-  
-      
-      // For now, Delete temporary pod here
-      // Don't know how to delete after Promise.all above
-      try {
-        const deletePod = await client.apis.apps.v1.namespaces('dist').deployments(canaryName).delete()
-      } catch (err) {
-        msg.send(err) 
-      }
-    } // --- End of upload dist
-
-    const newImage = {
-            "spec": {
-              "template": {
-                "spec": {
-                  "containers": [{
-                    "name": deployName,
-                    "image": version,
-                  }]
-                }
-              }
-            }
-          };
-    try {
-      deploy = await client.apis.apps.v1.namespaces('default').deployments(deployName).patch({body: newImage});
-      if (deploy.statusCode === 200) {
-        osCmd('kubectl', ['rollout', 'status', `deployment/${deployName}`, '-w'])
-        msg.send(`${deployName} updated`)
-      } else {
-        msg.send(`${deployName} update got status code: ${deploy.statusCode}`)
-      }
-
+      });
+      msg.send(`${deployName} updated`)
     } catch (err) {
       msg.send(`Update deployment ${deployName} error: `, err)
     }
   })
 
-  robot.respond(/upload\s+dist\s+rr\s+(readr-site-mobile|readr-site|news-projects|readr-restful)\s+(.+)/i, async msg => {
+  robot.respond(/upload\s+dist\s+dev\s+(readr-site-mobile|readr-site|news-projects)\s+(.+)/i, async msg => {
     
     const deployName = msg.match[1];
-    const version = "gcr.io/mirrormedia-1470651750304/" + deployName + ":" + msg.match[2];
-    const canaryName = deployName + "-canary"
+    const fullImage = "gcr.io/mirrormedia-1470651750304/" + deployName + ":" + msg.match[2];
+    const canaryName = `${deployName}-canary`
+    let distribution; 
 
     switch (deployName) {
       case "readr-site-mobile":
-        var distribution = "distribution"
+        distribution = "distribution"
         break;
       case "readr-site":
-        var distribution = "distribution"
+        distribution = "distribution"
         break;
       case "news-projects":
-        var distribution = "dist"
+        distribution = "dist"
         break;
     }
-
-    if (deployName !== "readr-restful"){
-      let canaryTemplate = {
-        apiVersion: "apps/v1",
-        kind: "Deployment",
-        metadata: {
-          name: canaryName,
-          namespace: "dist",
-          labels: {
-            app: canaryName
-          }
-        },
-        spec: {
-          replicas: 1,
-          selector: {
-              matchLabels: {
-              app: canaryName
-              }
-          },
-          template: {
-            metadata:{
-              labels: {
-                app: canaryName
-              }
-            },
-            spec: {
-              containers: [
-                {
-                  image: version,
-                  name: canaryName
-                }
-              ]
-            }
-          }
-        }
-      }
-      console.log(canaryTemplate)
-  
-      try {
-        const create = await client.apis.apps.v1.namespaces('dist').deployments.post({ body: canaryTemplate })
-      } catch (err) {
-        msg.send(err)
-      }
-    
-      // Watch the creation until it's done
-      osCmd('kubectl', ['rollout', 'status', `deployment/${canaryName}`, '-w', '-n', 'dist'])
-    
-      try {
-        const pod = await client.api.v1.namespaces('dist').pods.get({ qs: {labelSelector: `app=${canaryName}` }})
-        var podName = pod.body.items[0].metadata.name
-        console.log(`pod is named: ${podName}`)
-        // Copy dist file out
-        var distFolder = `/tmp/${podName}/${distribution}`
-        osCmd('kubectl', ['cp', `${podName}:/usr/src/${distribution}`, distFolder, '-n', 'dist'])
-      } catch (err) {
-        msg.send(err)
-      }
-    
-      const bucket = initBucket(GCS_FILE_BUCKET)
-      const destination = GCS_BUCKET_PATH
-      try {
-        files = await readdirAsync(distFolder)
-      } catch (err) {
-        msg.send(err)
-      }
-  
-      let result = await Promise.all(files.map( filename => {
-        return uploadFileToBucket(bucket, `${distFolder}/${filename}`, {
-          destination: `${destination}/${filename}`,
-        }).then((bucketFile) => {
-          console.log(`file ${filename} upload to gcs successfully.`)
-          makeFilePublic(bucketFile)
-        })
-      }))
-      .then(() => {
-        // Remove temporary folder
-        osCmd('rm', ['-rf', `/tmp/${podName}`])
-      })
-  
-      
-      // For now, Delete temporary pod here
-      // Don't know how to delete after Promise.all above
-      try {
-        const deletePod = await client.apis.apps.v1.namespaces('dist').deployments(canaryName).delete()
-      } catch (err) {
-        msg.send(err) 
-      }
-    } // --- End of upload dist
+    try {
+      await uploadDist("dist", canaryName, fullImage, distribution)
+      msg.send("dist uploaded.")
+    }catch (err) {
+      msg.send(err)
+    }
   })
 }
 
--- scripts/k8s.js
+++ scripts/k8s.js
@@ -1,3 +1,10 @@
+require('dotenv').config()
+
+// 
+const GCS_FILE_BUCKET = process.env.GCS_FILE_BUCKET
+const GCS_BUCKET_PATH = process.env.GCS_BUCKET_PATH
+const {initBucket, uploadFileToBucket, readdirAsync, makeFilePublic, } = require('./gcs.js');
+
 const kubernetesClient = require("kubernetes-client");
 
 const Client = kubernetesClient.Client;
@@ -7,7 +14,9 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
-const readFileAsync = util.promisify(fs.readFile);
+const readfileAsync = util.promisify(fs.readFile);
+
+exec = require('child_process').exec
 
 const client = new Client({
   config: process.env.NODE_ENV === "production" ? config.getInCluster() : config.fromKubeconfig(),
@@ -15,13 +24,13 @@ const client = new Client({
 });
 
 // List deploy in namespace
-const listDeploy = async (ns, deployName) => {
+const getDeployVersion = async (ns, deployName) => {
   
   let version
   
   try {
     const deploy = await client.apis.apps.v1.namespaces(ns).deployments(deployName).get();
-    console.log(deploy.statusCode)
+    console.log(`Get image version success. Status: ${deploy.statusCode}`)
     switch (deploy.statusCode)  {
       case 200:
         let containers = deploy.body.spec.template.spec.containers;
@@ -33,12 +42,12 @@ const listDeploy = async (ns, deployName) => {
         }
         break;
       default:
-        return "not found"
+        throw "not found"
     }
-    return version 
+    return version
 
   } catch (err) {
-    console.log(err)
+    throw `error status: ${err.statusCode}`
   }
 }
 
@@ -47,22 +56,22 @@ const replaceAll = (str, find, replace) => {
   return str.replace(new RegExp(find, 'g'), replace);
 }
 
-// Open canary-template.json, and use it to create a deployment "deployName" with image "Imagever"
-const createDeploy = async (ns, deployName, imageVer) => {
+// Open canary-template.json, and use it to create a "deployment" in "namespace" with new "version"
+const createCanary = async (namespace, deployment, version) => {
   // Prepare manifest
   let deployManifest
   
   try {
-    let template = await readFileAsync(path.resolve(__dirname, "../manifests/canary-template.json"), "utf8")
-    deployManifest = JSON.parse(replaceAll(template, "canaryName", deployName))
-    deployManifest.spec.template.spec.containers[0].image = imageVer
+    let template = await readfileAsync(path.resolve(__dirname, "../manifests/canary-template.json"), "utf8")
+    deployManifest = JSON.parse(replaceAll(template, "canaryName", deployment))
+    deployManifest.spec.template.spec.containers[0].image = version
   }catch (err) {
     console.log(err)
     return err
   }
   // create deployment
   try{
-    const create = await client.apis.apps.v1.namespaces(ns).deployments.post({body: deployManifest})
+    const create = await client.apis.apps.v1.namespaces(namespace).deployments.post({body: deployManifest})
     console.log(create.statusCode) // 201 if success
   }catch (err){
     console.log(err)
@@ -71,39 +80,152 @@ const createDeploy = async (ns, deployName, imageVer) => {
   return "create succeeded"
 }
 
-const uploadDist = async (ns, deployName) => {
-
-}
-
-// Upgrade image of deployments
-const patchDeployImage = async (ns, deployName, imageVer) => {
+const uploadDist = async (namespace, deployment, version, distribution) => {
   
   try {
+    let deploy = await client.apis.apps.v1.namespaces(namespace).deployments(deployment).get()
+    console.log(`deploy status: ${deploy.statusCode}`)
+    switch (deploy.statusCode) {
+      case 200:
+        // Scale canary to 1
+        try {
+          let patchCanary = await patchDeployment(namespace, deployment, {
+            body: {
+              spec: {
+                replicas: 1,
+                template: {
+                  spec: {
+                    containers: [{
+                      name: deployment,
+                      image: version
+                    }]
+                  }
+                }
+              }
+            }
+          });
+          console.log("scale news-project-canary to 1")
+        } catch (err) {
+          throw err
+        }
+        break
 
-    const updateImage = await client.apis.apps.v1.namespaces(ns).deployments(deployName).patch({
+      default:
+        throw 'error finding or creating canary'
+      }
+    } catch (err) {
+      console.log("finding news-projects-canary error")
+      try {
+        let deploy = await createCanary("dist", "news-projects-canary", version)
+        console.log(deploy)
+      }catch (err){
+        console.log(err)
+        throw err
+      }
+  } 
+  // Create delay for pods to be ready. There is a gap between status ready to actual ready
+  let sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  await sleep(3000);
+
+  // Get pod's name and copy dist
+  let canaryPod;
+  try {
+    const pod = await client.api.v1.namespaces(namespace).pods.get({ qs: {labelSelector: `app=${deployment}` }})
+    canaryPod = pod.body.items[0].metadata.name
+    console.log(`Found canary pod name: ${canaryPod}`)
+  } catch (err) {
+    throw err
+  }
+  
+
+  // Copy dist file out
+  let distFolder = `./dist/${canaryPod}`
+  try {
+    let { stdout, stderr } = await sh(`kubectl -n dist cp ${canaryPod}:/usr/src/${distribution} ${distFolder}`)
+    console.log(`kubectl cp: ${stdout}`);
+    console.log(`kubectl cp: ${stderr}`);
+  }catch (err) {
+    throw err
+  }
+
+  // Upload dist
+  const bucket = initBucket(GCS_FILE_BUCKET)
+  const destination = GCS_BUCKET_PATH
+  try {
+    files = await readdirAsync(distFolder)
+  } catch (err) {
+    throw err
+  }
+
+  let result = await Promise.all(files.map( filename => {
+    return uploadFileToBucket(bucket, `${distFolder}/${filename}`, {
+      destination: `${destination}/${filename}`,
+    }).then((bucketFile) => {
+      console.log(`file ${filename} upload to gcs successfully.`)
+      makeFilePublic(bucketFile)
+    }).catch((err) => {
+      console.log(`Error code: ${err.code}`)
+    }) 
+  }))
+  .then(async () => {
+    await sh(`rm -rf ${distFolder}`)
+    console.log("finished removing dist temp files")
+  }).catch( (err) => {
+    console.log(err)
+  })
+
+  try {
+    await patchDeployment(namespace, deployment, {
       body: {
         spec: {
-          template: {
-            spec: {
-              containers: [{
-                name: deployName,
-                image: imageVer
-              }]
-            }
-          }
+          replicas: 0
         }
       }
-    })
+    });
+    console.log(`scale ${deployment}:${namespace} to 0`)
+  } catch (err) {
+    throw err
+  }
+}
+
+// Patch a deployment with patchData, and watch it finishing the rollout
+const patchDeployment = async (namespace, name, patchData) => {
+  try {
+    const updateImage = await client.apis.apps.v1.namespaces(namespace).deployments(name).patch(patchData)
     console.log(updateImage.statusCode)
   } catch(err) {
     console.log(err)
     return err
   }
+  
+  // Watch the rolling-out
+  const checkInterval = 2000;
+  const checkTimeout = 30000;
+
+  let interval, timeout;
+
+  // Set Timeout for watch
+  timeout = setTimeout(async() => {
+    clearInterval(interval);
+    throw `Timeout: ${checkTimeout}`
+  }, checkTimeout);
+  
+  // Check deployment for every checkInterval
+  interval = setInterval(async() => {
+    const deployment = await client.apis.apps.v1.namespaces(namespace).deployments(name).get();
+    // There is no unavailableReplicas means all pods are available 
+    if (deployment.body.status.unavailableReplicas === undefined){
+      clearTimeout(timeout);
+      clearInterval(interval);
+      // console.log("rollout successful")
+      return "rollout successful"
+    }
+  }, checkInterval);
 }
 
 // Scale deployment in specified namespace
 const scaleDeploy = async (ns, deployName, rep) => {
-  
+  console.log(`scaleDeploy ${deployName} in ${ns} to ${rep}`)
   try {
     const scaleDeploy = await client.apis.apps.v1.namespaces(ns).deployments(deployName).patch({
       body: {
@@ -112,10 +234,11 @@ const scaleDeploy = async (ns, deployName, rep) => {
         }
       }
     })
+    console.log(scaleDeploy.body.status.conditions)
     return scaleDeploy.statusCode
   } catch (err) {
     console.log(err)
-    return err
+    throw `scale deployment ${ns}:${deployName} failed`
   }
 }
 
@@ -127,7 +250,35 @@ function osCmd(command, arguments) {
       console.log( `kubectl result: stderr: ${cmd.stderr.toString()}` )   
 }
 
-// module.exports = {
-//     client,
-//     osCmd
-// }
+// shell command
+async function sh(cmd) {
+  return new Promise( (resolve, reject) => {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ stdout, stderr })
+      }
+    })
+  })
+}
+
+const main = async () => {
+  try {
+    let deploy = await client.apis.apps.v1.namespaces("dist").deployments("news-projects-canary").get()
+    // let deploy = await client.api.v1.namespaces("dist").deployments("news-projects-canary").get()
+    if (deploy.statusCode === 200) {
+      console.log(deploy);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+// main()
+
+module.exports = {
+    client,
+    getDeployVersion,
+    uploadDist,
+    patchDeployment
+}
