781072bf5add5e6b3e4ab5e65891cdb76c5297ce
Leon Peng <leonpeng@mirrormedia.mg>
2020-05-05 16:58:46 +0800

    still add logs to find the correct way handling response

--- scripts/k8s.js
+++ scripts/k8s.js
@@ -235,9 +235,9 @@ const patchDeployment = async (namespace, name, patchData) => {
 };
 
 const getRevisions = async (namespace, deployname) => {
-    await sh(`kubectl rollout history deployment ${deployname} -n ${namespace}`);
-    console.log(stdout);
-    console.log(stdout.split('\n').slice(2).reverse());
+    const { stdout, stderr } = await sh(`kubectl rollout history deployment ${deployname} -n ${namespace}`);
+    console.log(stdout.split('\n'));
+    console.log(stdout.split('\n').slice(2).reverse())
     revisions = stdout.split('\n').slice(2).reverse().flatMap(async rev => {
         rev = rev.split(' ')[0];
         console.log(rev);
