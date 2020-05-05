8cf1b956da0d34d0483ef95b5f1f65fc28d6680c
Leon Peng <leonpeng@mirrormedia.mg>
2020-05-05 19:12:32 +0800

    handle rejection error

--- scripts/k8s.js
+++ scripts/k8s.js
@@ -238,25 +238,30 @@ const getRevisions = async (namespace, deployname) => {
     const { stdout, stderr } = await sh(`kubectl rollout history deployment ${deployname} -n ${namespace}`);
     console.log(stdout.replace(/\n\s+/m, '').split('\n'));
     console.log(stdout.replace(/\n\s+/m, '').split('\n').slice(2).reverse());
-    revisions = stdout.replace(/\n\s+/m, '').split('\n').slice(2).reverse().flatMap(async rev => {
-        rev = rev.split(/ +/)[0];
-        console.log(rev);
-        console.log('exec', `kubectl rollout history deployment ${deployname} -n ${namespace} --revision=${rev}`);
-        const { stdout, stderr } = await sh(`kubectl rollout history deployment ${deployname} -n ${namespace} --revision=${rev}`);
-        console.log(stdout);
-        const re = RegExp(`${deployname}:\\s+Image:\\s+.*`, 'm');
-        image = stdout.match(re)[0];
-        console.log(image);
-        if (image) {
-            tag = image.slice(image.lastIndexOf(':') + 1);
-            console.log(tag);
-            return `${rev}\\t${tag}`;
-        }
 
-        return `${rev}\\t`;
-    });
+    try {
+        revisions = stdout.replace(/\n\s+/m, '').split('\n').slice(2).reverse().flatMap(async rev => {
+            rev = rev.split(/ +/)[0];
+            console.log(rev);
+            console.log('exec', `kubectl rollout history deployment ${deployname} -n ${namespace} --revision=${rev}`);
+            const { stdout, stderr } = await sh(`kubectl rollout history deployment ${deployname} -n ${namespace} --revision=${rev}`);
+            console.log(stdout);
+            const re = RegExp(`${deployname}:\\s+Image:\\s+.*`, 'm');
+            image = stdout.match(re)[0];
+            console.log(image);
+            if (image) {
+                tag = image.slice(image.lastIndexOf(':') + 1);
+                console.log(tag);
+                return `${rev}\\t${tag}`;
+            }
 
-    return revisions;
+            return `${rev}\\t`;
+        });
+
+        return revisions;
+    } catch (error) {
+        throw error;
+    }
 };
 
 // shell command
