b95c44fa651db6d75257a96e39af8ffb70092e3b
Leon Peng <leonpeng@mirrormedia.mg>
2020-05-05 18:21:04 +0800

    replace split pattern

--- scripts/k8s.js
+++ scripts/k8s.js
@@ -237,7 +237,7 @@ const patchDeployment = async (namespace, name, patchData) => {
 const getRevisions = async (namespace, deployname) => {
     const { stdout, stderr } = await sh(`kubectl rollout history deployment ${deployname} -n ${namespace}`);
     revisions = stdout.replace(/\n\s+/m, '').split('\n').slice(2).reverse().flatMap(async rev => {
-        rev = rev.replace(/\s+/, ' ').split(' ')[0];
+        rev = rev.split(/ +/)[0];
         console.log(rev);
         console.log('exec', `kubectl rollout history deployment ${deployname} -n ${namespace} --revision=${rev}`);
         const { stdout, stderr } = await sh(`kubectl rollout history deployment ${deployname} -n ${namespace} --revision=${rev}`);
